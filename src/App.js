5870767719efe06fec8e054ae3763a3c8e95cde3
HasnainAsif <hasnainasif52@gmail.com>
2022-04-28 14:58:14 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { SERVER_URL } from './config';
 
 function App() {
+  const [data, setData] = useState('');
+
   useEffect(() => {
     fetch(SERVER_URL)
       .then(async (res) => {
         const data = await res.json();
         if (res.status === 200) {
           console.log(data.data);
+          setData(data.data);
         } else {
+          setData(data.msg);
           console.log(data.msg || 'error occured...');
         }
       })
       .catch((err) => {
+        setData(err.message || 'error occured...');
         console.log(err.message || 'error occured...');
       });
   }, []);
@@ -34,7 +39,7 @@ function App() {
         >
           Learn React
         </a>
-        <p>Look at console</p>
+        <p>API Response: {data}</p>
       </header>
     </div>
   );
